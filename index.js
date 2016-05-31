47b2462793f900dc6082330ab0abc23d070daaaf
hambakas <hambakas@hotmail.com>
2016-05-31 21:18:33 +0700

    first

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express'); 
+var cors = require('cors'); 
+var app = express(); 
+app.use(cors()); 
+ 
+app.get('/', function (req, res) { 
+  res.send("hello final2"); 
+}); 
+ 
+ 
+ app.get('/:number', function (req, res) { 
+  var result = { 
+  data: parseInt(req.params.number, 10) * 2 
+  }; 
+  res.send(result); 
+}); 
+ 
+ 
+app.listen(process.env.PORT || 3000, function () { 
+console.log('Example app listening on port 3000!'); 
+console.log('testv2'); 
+}); 
